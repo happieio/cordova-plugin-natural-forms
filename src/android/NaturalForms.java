0990b24fe95a478c8c7f8ae3e46ece4dfe351dda
James Spencer <thisjustosay@gmail.com>
2016-12-14 15:09:44 -0800

    improve error handling for android

--- src/android/NaturalForms.java
+++ src/android/NaturalForms.java
@@ -15,38 +15,43 @@ import java.io.IOException;
 
 public class NaturalForms extends CordovaPlugin {
 
-	public static final String TAG = "NaturalForms";
+    public static final String TAG = "NaturalForms";
 
-	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callback) throws JSONException {
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callback) throws JSONException {
 
-		final Intent nfIntent = cordova.getActivity().getPackageManager().getLaunchIntentForPackage("net.expedata.naturalforms");
-		final CallbackContext finalCallBack = callback;
+        final Intent nfIntent = cordova.getActivity().getPackageManager().getLaunchIntentForPackage("net.expedata.naturalforms");
+        final CallbackContext finalCallBack = callback;
 
-		if(nfIntent == null) {
-			callback.error("Please install naturalForms");
-		}
+        if (nfIntent == null) {
+            callback.error("Please install naturalForms");
+        }
 
-		File csv = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/JobNimbus/", "data.csv");
-		try{
-			FileWriter fw = new FileWriter(csv);
-			fw.write(args.getString(0));
-			fw.close();
-		} catch(IOException ioe){
-			callback.error("Could not save naturalForms data.");
-		}
+        File csv = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/JobNimbus/", "data.csv");
+        try {
+            FileWriter fw = new FileWriter(csv);
+            fw.write(args.getString(0));
+            fw.close();
+        } catch (IOException ioe) {
+            callback.error("Could not save naturalForms data.");
+        }
 
-		nfIntent.setData(Uri.fromFile(csv));
-		nfIntent.setClassName("net.expedata.naturalforms", "net.expedata.naturalforms.ui.CsvImportActivity"); // , csvImport.name);
-		// startActivity(nfIntent);
+        nfIntent.setData(Uri.fromFile(csv));
+        try {
+            nfIntent.setClassName("net.expedata.naturalforms", "net.expedata.naturalforms.ui.CsvImportActivity"); // , csvImport.name);
 
-		final NaturalForms plugin = this;
-		cordova.getThreadPool().execute(new Runnable() {
-			public void run() {
-				cordova.startActivityForResult(plugin, nfIntent, 0);
-				finalCallBack.success("success");
-			}
-		});
-		return true;
-	}
+            final NaturalForms plugin = this;
+            cordova.getThreadPool().execute(new Runnable() {
+                public void run() {
+                    cordova.startActivityForResult(plugin, nfIntent, 0);
+                    finalCallBack.success("success");
+                }
+            });
+            return true;
+        } catch (Exception err) {
+            callback.error("naturalForms is not supported on this device");
+            return true;
+        }
+        // startActivity(nfIntent);
+    }
 }
