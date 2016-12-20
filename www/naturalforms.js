5d0db8287b5a77402f3a8d283765d31b3dd7eb31
James Spencer <thisjustosay@gmail.com>
2016-12-20 12:01:04 -0800

    fix casing error in cordova bridge

--- www/naturalforms.js
+++ www/naturalforms.js
@@ -2,6 +2,6 @@ var exec = require('cordova/exec');
 
 module.exports = {
     sendToNaturalForms: function (res, err, params) {
-        cordova.exec(res, err, "naturalForms", "launchNaturalForms", params);
+        cordova.exec(res, err, "NaturalForms", "launchNaturalForms", params);
     }
 };
